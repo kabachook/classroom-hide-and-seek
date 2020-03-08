977d4ef59507cb5bb0b19b5cd69f85927db527c8
Danil Beltyukov <beltyukovdanil@gmail.com>
2020-03-08 07:52:58 +0300

    backend: make app not run without env vars set

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,5 +1,5 @@
-// const dotenv = require("dotenv");
 import dotenv from "dotenv";
+import logger from "../loaders/logger";
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
@@ -8,6 +8,22 @@ if (!envFound) {
   throw new Error("⚠ Couldn't find .env file!");
 }
 
-export default {
-  port: parseInt(process.env.PORT, 10) || 3000
+const parsedEnv = {
+  port: parseInt(process.env.PORT, 10) || 3000,
+  travisciKey: process.env.TRAVISCI_KEY
 };
+
+let willExit = false;
+
+for (const [key, value] of Object.entries(parsedEnv)) {
+  if (!value) {
+    logger.fatal(`${key} is not set`);
+    willExit = true;
+  }
+}
+
+if (willExit) {
+  process.exit(1);
+}
+
+export default parsedEnv;
