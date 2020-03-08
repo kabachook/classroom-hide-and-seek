ac4be769a535d71f5a582f408072feffa6a54446
Danil Beltyukov <beltyukovdanil@gmail.com>
2020-03-08 19:34:56 +0300

    backend: remove redis loader

--- backend/src/loaders/redis.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "express";
-import Redis from "ioredis";
-import config from "../config";
-
-export default function({ app: Application }) {
-  const redis = new Redis(config.redisUrl);
-}
