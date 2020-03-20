637a779b666719edf199865df3e374691d1c9e3b
Danil Beltyukov <beltyukovdanil@gmail.com>
2020-03-20 22:56:16 +0300

    backend: enable rule routes

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import githubWebhook from "./routes/githubWebhook";
+import rule from "./routes/rule";
 
 export default () => {
   const app = Router();
   githubWebhook(app);
+  rule(app);
 
   return app;
 };
